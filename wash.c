38e4a435d97e4c195c3f667254a2b101ba70e6ea
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-02-13 21:29:26 -0500

    additions

--- wash.c
+++ wash.c
@@ -1,11 +1,9 @@
 #include "stdio.h"
+
 // This is my code
 // Jen Moutenot
 // CS366
 
-/* push.c */
-
-#include <stdio.h>
 
 const int BUF_SIZE = 512;
 
@@ -14,12 +12,15 @@ int main(int argc, char *argv[])
   for(argv++; *argv != NULL; )
   {
     char command[BUF_SIZE];
-    printf("\nfor the file ``%s'': please enter a command command: ", *argv);
+    printf("\nfile %s: command:", *argv);
     fgets(command, BUF_SIZE, stdin);
-    printf("File name is %s and the command you entered starts with a %c\n", *argv, *command);
-    argv++;
+
+    switch (*command)
+    {
+      case 'c': cat(*argv); break;
+...
+    }
   }
 
   return 0;
 }
-
