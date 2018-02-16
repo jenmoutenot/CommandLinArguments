2ab45178a119d6573322a10b488d1093df5fb6ae
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-02-15 19:44:31 -0500

    hi

--- wash.c
+++ wash.c
@@ -1,27 +1,81 @@
 #include "stdio.h"
 #include "stdlib.h"
-
+#include "unistd.h"
 // This is my code
 // Jen Moutenot
 // CS366
 
-
 const int BUF_SIZE = 512;
-/*
-void rename(const char *old, const char *new)
+
+/* purpose:
+ * input:
+ * returns:
+*/
+void unlink(char *file)
 {
-  gets_s(*old);
-  printf("Enter new name for file: ");
-  gets_s(*new);
-  if (rename(*old, *new) == 0 )
+  
+  
+}
+
+/* purpose:
+ * input:
+ * returns:
+*/
+void append(char *file) 
+{
+  char c;
+  char dest[BUF_SIZE];
+  printf("Enter file you would like to append to: \n");
+  scanf("%s", dest);
+  FILE *fp1 = fopen(file, "r");
+  FILE *fp2 = fopen(dest, "a");
+  while ((c = getc(fp1))!= EOF)
   {
-    printf("%s has been renamed %s.\n", *old, *new);
-  }
-   else 
+    putc(c, fp2);
+  }  
+}
+/* purpose:
+ * input:
+ * returns:
+*/
+void duplicate(char *file)
+{
+  char c;
+  char dest[BUF_SIZE];
+  
+  printf("Enter destination name: \n");
+  scanf("%s", dest);
+  FILE *fp1 = fopen(file, "r");
+  FILE *fp2 = fopen(dest, "w");
+  while ((c = getc(fp1))!= EOF)
   {
-    printf("Error\n");
+    putc(c, fp2);
   }
 }
+
+/* purpose:
+ * input:
+ * returns:
+*/
+void rename_file(char *file_old)
+{
+  char file_new[BUF_SIZE]; //technically a pointer
+  
+  printf("Enter new name for file: \n");
+  scanf("%s", file_new);
+  if (rename(file_old, file_new) == 0 ) //file_old is still a pointer, was not dereferenced
+  {
+    printf("%s has been renamed %s.\n", file_old, file_new); //passing through strings
+  }
+    else 
+  {
+     printf("Error\n");
+  }
+}
+
+/* purpose:
+ * input:
+ * returns:
 */
 void cat(char *file)
 {
@@ -29,14 +83,17 @@ void cat(char *file)
   fp = fopen(file, "r"); //fp is the file pointer
   char c;  
   c = fgetc(fp);  
-  while( c != EOF) //end of file
+  while(c != EOF) //end of file
   {
      printf("%c", c); //printing the contents of the file
      c = fgetc(fp); 
   }
 }
 
-
+/* purpose:
+ * input:
+ * returns:
+*/
 
 int main(int argc, char *argv[])
 {
@@ -67,14 +124,18 @@ int main(int argc, char *argv[])
     switch (*command)
     {
       case 'c': cat(*argv); break;
-      /*case 'd': duplicate(*argv); break;
-      case 'r': rename(*argv); break;
+      case 'd': duplicate(*argv); break;
+      case 'r': rename_file(*argv); break;
+    /*
       case 'u': unlink(*argv); break;
       case 't': truncate(*argv); break;
+    */
       case 'a': append(*argv); break;
+    /*
       case 'l': last(*argv); break;
       case 'm': mode(*argv); break;
-      case 'x': time(*argv); break; */
+      case 'x': time(*argv); break;
+    */
       case 'n': argv++; break; 		//incrementing to the next file
       case 'q': return 0; break; 
     }
